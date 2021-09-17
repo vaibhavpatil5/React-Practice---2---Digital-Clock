d81314271d4e78904c984bdb2474fb87373c92db
Vaibhav Patil <65767045+vaibhavpatil5@users.noreply.github.com>
2021-09-17 20:11:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,40 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-function getCurrentTime(){
-    const currentDate= new Date();
-    return currentDate.toLocalTimeString().toUpperCase();
+
+function getCurrentTime() {
+    const currentTime = new Date();
+    // const hours = currentTime.getHours();
+    // const minutes = currentTime.getMinutes();
+    // const seconds = currentTime.getSeconds();
+
+    // const realTime = `${hours}:${minutes}:${seconds}`;
+    // return realTime;
+    return currentTime.toLocaleTimeString();
 }
 
-
 class App extends Component {
-    
     constructor(props){
         super(props);
-        this.state={
-              time:getCurrentTime(),     
+        this.state = {
+            time : getCurrentTime(),
         };
     }
-    
-   componentDidMount(){
-        this.xyz = setInterval(()=>{
-            this.setState({time:getCurrentTime()});
-        },1000);
-   } 
-    
-    componentWillUnmount(){
-        clearInterval(this.xyz);
-    }
-    
-    render() {
 
+    componentDidMount() {
+        this.intervalId = setInterval(() => {
+            this.setState({ time: getCurrentTime() });
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+    }
+    render() {
         return(
-            <>
-           <div className="Clock">
-                <h3 id="time">{this.state.time}</h3>
-            </div>
-            </>
+         <div class="Clock">
+             <h3 id="time">{this.state.time}</h3>
+         </div>
         )
     }
 }
