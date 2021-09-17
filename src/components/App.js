49d52a730bf211720dc5bda02ca82f4f09030384
Vaibhav Patil <65767045+vaibhavpatil5@users.noreply.github.com>
2021-09-17 16:53:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,19 @@ import '../styles/App.css';
 
 function getCurrentTime(){
     const currentDate= new Date();
-    const hours = currentDate.getHours();
-    const mins = currentDate.getMinutes();
-    const secs = currentDate.getSeconds();
+//     const hours = currentDate.getHours();
+//     const mins = currentDate.getMinutes();
+//     const secs = currentDate.getSeconds();
     
-    return hours + ':' + mins + ':' + secs;
+//     return hours + ':' + mins + ':' + secs;
+    return currentDate.toLocalTimeString().toUpperCase();
 }
 
 
 class App extends Component {
     
     constructor(props){
+        this.currentDate= new Date();
         super(props);
         this.state={
               time:getCurrentTime(),     
@@ -21,10 +23,14 @@ class App extends Component {
     }
     
    componentDidMount(){
-        setInterval(()=>{
+        this.intervalId = setInterval(()=>{
             this.setState({time:getCurrentTime()});
-        });
+        },1000);
    } 
+    
+    componentWillUnmount(){
+        clearInterval(this.intervalId);
+    }
     
     render() {
 
